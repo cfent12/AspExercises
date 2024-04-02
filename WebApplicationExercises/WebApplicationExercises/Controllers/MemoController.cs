764b2f498e20627504800aaa4b3dc15349b63e8a
최경호 <cfent12@thirautech.com>
2024-04-02 09:12:57 +0900

    ASP.net MVC 프로젝트 업로드

---
+++ WebApplicationExercises/WebApplicationExercises/Controllers/MemoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using WebApplicationExercises.Models;
+
+namespace WebApplicationExercises.Controllers
+{
+    public class MemoController : Controller
+    {
+        private static List<Memo> memos = new List<Memo>();
+        // GET: Memo
+        public ActionResult Index()
+        {
+            Memo tempMemo = new Memo { Title = "titleee", Contant = "<h1>Memo Test</h1>", Id = 1 };
+            memos.Add(tempMemo);
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            Memo tempMemo = new Memo { Title = "titleee", Contant = "<h1>Memo Test</h1>", Id = 1 };
+            memos.Add(tempMemo);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Memo memo)
+        {
+            memos.Add(memo);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplicationExercises/WebApplicationExercises/Models/Memo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationExercises.Models
+{
+    public class Memo
+    {
+        public string Title { get; set; }
+        public string Contant { get; set; }
+        public int Id { get; set; }
+    }
+}
