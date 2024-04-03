d6bfc6d4848d5d0d865ba2834e1f5a4f51259e0f
최경호 <cfent12@thirautech.com>
2024-04-03 16:52:15 +0900

    controller, view, model 추가

---
+++ WebApplicationExercises/WebApplicationExercises/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationExercises.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 첫번째 메서드
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: HelloWorld/Welcome/
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
